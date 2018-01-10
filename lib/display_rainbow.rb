eb6a4e6d8dce9afad955bf37f8a560c063c05d8a
Devin Wilson <craigvin84@yahoo.com>
2018-01-10 22:29:58 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,4 @@
-def display_rainbow(['red' , 'orange' , 'yellow' , 'green' , 'blue' , 'indigo' , 'violet'])
+def display_rainbow do
+  colors = (['red' , 'orange' , 'yellow' , 'green' , 'blue' , 'indigo' , 'violet'])
   R = display_rainbow[0]
 end
